21124234322806658139dff2c21c6df5e03e697f
Junaid Rasheed <jrasheed@proda.ai>
2024-09-19 17:21:26 +0100

    Look for a 422 with a non-zero exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,9 +14,10 @@ fi
 
 # Attempt to create the release.
 RESPONSE=$(gh release create "$INPUT_TAG" -t "${INPUT_TITLE}" --generate-notes 2>&1)
+STATUS=$?
 
-# Check if the body is too long.
-if echo "$RESPONSE" | grep -q "body is too long"; then
+# Check if the release creation failed due to body being too long (HTTP 422).
+if [ $STATUS -ne 0 ] && echo "$RESPONSE" | grep -q "422"; then
   echo "Release notes are too long. Publishing again with release notes as a file."
 
   # Write the release notes to a file.
