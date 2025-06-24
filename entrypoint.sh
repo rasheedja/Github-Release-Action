be86c181b610497ba1f6df9068c50795c3f9756c
Junaid Ali Rasheed <jrasheed178@gmail.com>
2025-06-24 13:26:21 +0100

    fix: simplify 'too many chars' check (#9)

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,26 +10,21 @@ if [ -z "${INPUT_TAG}" ]; then
   INPUT_TAG="release-$(date +%Y%m%d%H%M%S)"
 fi
 
-# Capture notes in a variable to potentially write them to a file later.
+RELEASE_NOTES=$(gh api repos/:owner/:repo/releases/generate-notes -f tag_name="$INPUT_TAG" --jq '.body')
 
-# Attempt to create the release.
-RESPONSE=$(gh release create "$INPUT_TAG" -t "${INPUT_TITLE}" --generate-notes 2>&1)
-STATUS=$?
+# Check if release notes exceeds GH char limit (currently 125k characters)
+# We'll use 124k characters in case there are slight difference in the way
+# GitHub calculates the character count.
+NOTES_LENGTH=$(printf "%s" "$RELEASE_NOTES" | wc -c)
 
-# Check if the release creation failed due to body being too long (HTTP 422).
-if [ $STATUS -ne 0 ] && echo "$RESPONSE" | grep -q "422"; then
-  echo "Release notes are too long. Publishing again with release notes as a file."
-
-  # Fetch the release details.
-  RELEASE=$(gh release view --json body,name,id,tagName)
-  TAG_NAME=$(printf "%s" "$RELEASE" | jq -r ".tagName")
-  RELEASE_NOTES=$(printf "%s" "$RELEASE" | jq -r ".body")
-
-  # Write the release notes to a file.
-  echo "$RELEASE_NOTES" > "${TAG_NAME}.md"
-
-  # Retry release creation with release notes attached as a markdown file.
-  gh release create "$INPUT_TAG" -t "${INPUT_TITLE}" --notes-file "${TAG_NAME}".md
+if [ "$NOTES_LENGTH" -gt 124000 ]; then
+  echo "Release notes are ${NOTES_LENGTH} characters. Creating release with notes as attached file."
+  
+  echo "$RELEASE_NOTES" > "release-notes.md"
+  
+  gh release create "$INPUT_TAG" -t "${INPUT_TITLE}" --notes-file "release-notes.md"
 else
-  echo "$RESPONSE"
+  echo "Release notes are ${NOTES_LENGTH} characters. Creating release with notes in body."
+  
+  gh release create "$INPUT_TAG" -t "${INPUT_TITLE}" --notes "$RELEASE_NOTES"
 fi
